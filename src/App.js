0d04ba98fd6721e915877d22ab2c9e73572f9a07
mayurL <mayur5643@gmail.com>
2021-03-23 23:27:04 +0530

    Styled ‘Header’ (Inline Styling)

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ class App extends Component{
 render() {
   return (
       <div>
-      <Header/>
-      <button> Add </button>
-      <div>
-      <span> Name </span><br/>
-      <span> Phone </span>
+        <Header/>
+          <button> Add </button>
+        <div>
+         <span> Name </span><br/>
+         <span> Phone </span>
+        </div>
+        {/* <label htmlFor="name"> Name: </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="Mayur"/> */}
       </div>
-      {/* <label htmlFor="name"> Name: </label>
-      <input id="name" type="text" placeholder="Type Here" defaultValue="Mayur"/> */}
-    </div>
   );
 }
 }
